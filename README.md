86a4ac3e5d1709c83643f243f60145d30bd61622
burravlev <prodglockem@gmail.com>
2023-11-03 17:14:45 +0300

    Update

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Simple golang parser for retrieving video metadata.
 client := youtube.Client{}
 video, err := client.GetVideo("abc12345678")
 ```
+### Download
+```go
+file, err := client.Download(youtube.Request{
+	Filepath: "audio",
+	Format:   &video.Formats[0],
+	// on downloading callback function
+	Callback: func(percent int) {
+		fmt.Println(percent)
+        }   
+})
+```
--- info.go
+++ info.go
@@ -1,5 +1,10 @@
 package youtube
 
+import (
+	"fmt"
+	"strings"
+)
+
 type VideoInfo struct {
 	VideoDetails  VideoDetails  `json:"videoDetails"`
 	StreamingData StreamingData `json:"streamingData"`
@@ -8,15 +13,18 @@ type VideoInfo struct {
 type StreamingData struct {
 	ExpiresInSeconds string   `json:"expiresInSeconds"`
 	Formats          []Format `json:"formats"`
+	AdaptiveFormats  []Format `json:"adaptiveFormats"`
 }
 
 type Format struct {
 	Itag         int    `json:"itag"`
 	URL          string `json:"url"`
 	MimeType     string `json:"mimeType"`
+	Codec        string `json:"-"`
 	Width        int    `json:"width"`
 	Height       int    `json:"height"`
 	QualityLabel string `json:"qualityLabel"`
+	AudioQuality string `json:"audioQuality"`
 }
 
 type VideoDetails struct {
@@ -34,4 +42,40 @@ type Thumb struct {
 	URL    string `json:"url"`
 	Width  int    `json:"width"`
 	Height int    `json:"height"`
+}
+
+func (v VideoInfo) BestAudioFormat() (*Format, error) {
+	formats := v.StreamingData.Formats
+	if formats == nil {
+		return nil, fmt.Errorf("cannot get audio formats")
+	}
+	temp := make([]Format, len(formats))
+	for _, format := range formats {
+		if strings.HasPrefix(format.MimeType, "audio") {
+			temp = append(temp, format)
+		}
+	}
+	if v.StreamingData.AdaptiveFormats != nil {
+		for _, format := range v.StreamingData.AdaptiveFormats {
+			if strings.HasPrefix(format.MimeType, "audio") {
+				temp = append(temp, format)
+			}
+		}
+	}
+	for _, format := range temp {
+		if format.AudioQuality == "AUDIO_QUALITY_HIGH" {
+			return &format, nil
+		}
+	}
+	for _, format := range temp {
+		if format.AudioQuality == "AUDIO_QUALITY_MEDIUM" {
+			return &format, nil
+		}
+	}
+	for _, format := range temp {
+		if format.AudioQuality == "AUDIO_QUALITY_LOW" {
+			return &format, nil
+		}
+	}
+	return nil, fmt.Errorf("cannot get audio format")
 }
